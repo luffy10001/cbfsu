df6a38a41d9ecdffbfef1a57d6cdd1e61b8d87dd
mudasirhamid <hamid.creativetech@gmail.com>
2024-10-03 06:29:54 +0500

    mm

--- database/migrations/2024_10_03_062905_change_column_status_into_bonds.php
+++ database/migrations/2024_10_03_062905_change_column_status_into_bonds.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-//        Schema::table('bonds', function (Blueprint $table) {
-//            $table->dropColumn('delivery_method');
-//        });
-//        Schema::table('bonds', function (Blueprint $table) {
-//            $table->text('delivery_method')->nullable();
-//        });
-
-        //DB::statement('ALTER TABLE bonds ALTER COLUMN delivery_method TYPE TEXT');
+        Schema::table('bonds', function (Blueprint $table) {
+            $table->dropColumn('delivery_method');
+        });
+        Schema::table('bonds', function (Blueprint $table) {
+            $table->text('delivery_method')->nullable();
+        });
     }
 
     /**
